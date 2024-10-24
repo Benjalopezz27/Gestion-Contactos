4875fa31249f1d8df6c2a4196013a80036c9a5cb
benjamin lopez <benjaminlopezzigaran01@gmail.com>
2024-10-24 11:40:19 -0300

    Translator Complete

--- GestionContactos.c
+++ GestionContactos.c
@@ -12,6 +12,9 @@ typedef struct
 void agregarContacto();
 void mostrarContactos();
 void eliminarContacto();
+void modificarContacto();
+int compararContactos();
+void ordenarContactos();
 
 int main()
 {
@@ -23,7 +26,9 @@ int main()
         printf("1 - Agregar contacto.\n");
         printf("2 - Mostrar contactos.\n");
         printf("3 - Eliminar contacto.\n");
-        printf("4 - Salir.\n");
+        printf("4 - Modificar contacto.\n");
+        printf("5 - Ordenar contactos.\n");
+        printf("6 - Salir.\n");
         printf("Ingrese la opcion: ");
         scanf("%d", &opcion);
         getchar();
@@ -40,14 +45,19 @@ int main()
             eliminarContacto();
             break;
         case 4:
+            modificarContacto();
+            break;
+        case 5:
+            ordenarContactos();
+            break;
+        case 6:
             printf("¿Está seguro que desea salir?\n");
             break;
-
         default:
             printf("Error. Intente nuevamente.\n");
             break;
         }
-    } while (opcion != 4);
+    } while (opcion != 6);
 }
 
 void agregarContacto()
@@ -91,7 +101,7 @@ void mostrarContactos()
     printf("---- Lista de Contactos ----\n");
     while (fread(&contacto, sizeof(contacto), 1, arch))
     {
-        printf("--Nombre: %s--Telefono: %s--Mail: %s--\n", contacto.nombre, contacto.numero, contacto.mail);
+        printf("----Nombre: %s --Telefono: %s --Mail: %s ---- \n", contacto.nombre, contacto.numero, contacto.mail);
     }
     fclose(arch);
 }
@@ -117,7 +127,7 @@ void eliminarContacto()
     char nombre[50];
     Contacto contacto;
 
-    printf("Ingrese el nombre del contacto a eliminar:\n");
+    printf("Ingrese el nombre del contacto a eliminar: \n");
     fgets(nombre, sizeof(nombre), stdin);
     nombre[strcspn(nombre, "\n")] = '\0';
 
@@ -147,4 +157,124 @@ void eliminarContacto()
     {
         printf("Contacto no encontrado.\n");
     }
+}
+
+void modificarContacto()
+{
+    int opcion;
+    char nombre[50];
+    Contacto contacto;
+    FILE *arch = fopen("contactos.dat", "rb");
+    FILE *temporal = fopen("temporal.dat", "wb");
+    if (!arch || !temporal)
+    {
+        printf("No se pudo abrir el archivo.\n");
+        return;
+    }
+
+    printf("Ingrese el nombre del contacto a modificar: ");
+    fgets(nombre, sizeof(nombre), stdin);
+    nombre[strcspn(nombre, "\n")] = '\0';
+
+    int encontrado = 0;
+
+    while (fread(&contacto, sizeof(contacto), 1, arch))
+    {
+        if (strcmp(nombre, contacto.nombre))
+        {
+            encontrado = 1;
+            printf("Ingrese el numero correspondiente (1 - nombre, 2 - telefono, 3 - mail): ");
+            scanf("%d", &opcion);
+            getchar();
+
+            switch (opcion)
+            {
+            case 1:
+                printf("Ingrese el nuevo nombre: ");
+                fgets(contacto.nombre, sizeof(contacto.nombre), stdin);
+                contacto.nombre[strcspn(contacto.nombre, "\n")] = '\0';
+                break;
+            case 2:
+                printf("Ingrese el nuevo telefono: ");
+                fgets(contacto.numero, sizeof(contacto.numero), stdin);
+                contacto.numero[strcspn(contacto.numero, "\n")] = '\0';
+                break;
+            case 3:
+                printf("Ingrese el nuevo mail: ");
+                fgets(contacto.mail, sizeof(contacto.mail), stdin);
+                contacto.mail[strcspn(contacto.mail, "\n")] = '\0';
+                break;
+            default:
+                printf("Opcion no valida\n");
+                break;
+            }
+        }
+        fwrite(&contacto, sizeof(contacto), 1, temporal);
+    }
+
+    fclose(arch);
+    fclose(temporal);
+
+    if (encontrado)
+    {
+        remove("contactos.dat");
+        rename("temporal.dat", "contactos.dat");
+        printf("Contacto modificado exitosamente.\n");
+    }
+    else
+    {
+        printf("Contacto no encontrado.\n");
+        remove("temporal.dat");
+    }
+}
+int compararContactos(const void *a, const void *b)
+{
+    Contacto *contactoA = (Contacto *)a;
+    Contacto *contactoB = (Contacto *)b;
+    return strcmp(contactoA->nombre, contactoB->nombre);
+}
+void ordenarContactos()
+{
+
+    FILE *arch = fopen("contactos.dat", "rb");
+    if (!arch)
+    {
+        printf("No es posible abrir el archivo.\n");
+        exit(1);
+    }
+
+    // Contar el número de contactos
+    fseek(arch, 0, SEEK_END);
+    long tamano = ftell(arch);
+    int numContactos = tamano / sizeof(Contacto);
+    fseek(arch, 0, SEEK_SET);
+
+    // Crear un array para almacenar los contactos
+    Contacto *contactos = malloc(numContactos * sizeof(Contacto));
+    if (!contactos)
+    {
+        printf("Error al asignar memoria.\n");
+        fclose(arch);
+        exit(1);
+    }
+
+    // Leer todos los contactos del archivo
+    fread(contactos, sizeof(Contacto), numContactos, arch);
+    fclose(arch);
+
+    // Ordenar los contactos
+    qsort(contactos, numContactos, sizeof(Contacto), compararContactos);
+
+    // Volver a escribir los contactos ordenados en el archivo
+    arch = fopen("contactos.dat", "wb");
+    if (!arch)
+    {
+        printf("No es posible abrir el archivo para escritura.\n");
+        free(contactos);
+        exit(1);
+    }
+
+    fwrite(contactos, sizeof(Contacto), numContactos, arch);
+    fclose(arch);
+    free(contactos);
 }
--- compilatorInSpanish/compilator.c
+++ compilatorInSpanish/compilator.c
@@ -2,15 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-void main()
+int main()
 {
-    FILE *filePointer;
-    filePointer = fopen("hello-world.txt", "r");
-
     char line[1024];
+    char newLine[1024];
+    FILE *filePointer;
+    FILE *tempFile;
 
-    printf("La frase es: ");
-    fgets(line, 1024, filePointer);
-    printf("%s", line);
+    filePointer = fopen("compilator.txt", "r");
+    if (!filePointer)
+    {
+        printf("No se puede abrir el archivo.\n");
+        exit(1);
+    }
+
+    tempFile = fopen("temp.txt", "w");
+    if (!tempFile)
+    {
+        fclose(filePointer);
+        printf("No se puede abrir el archivo temporal.\n");
+        exit(1);
+    }
+
+    while (fgets(line, sizeof(line), filePointer) != NULL)
+    {
+        strcpy(newLine, "");
+
+        if (strstr(line, "#incluir") != NULL)
+        {
+            strcat(newLine, "#include <stdio.h>\n");
+        }
+        else if (strstr(line, "entero principal()") != NULL)
+        {
+            strcat(newLine, "int main()\n ");
+        }
+
+        else if (strstr(line, "imprimir") != NULL)
+        {
+            strcat(newLine, "printf");
+            strcat(newLine, strchr(line, '('));
+        }
+        else if (strstr(line, "leer") != NULL)
+        {
+            strcat(newLine, "scanf");
+            strcat(newLine, strchr(line, '('));
+        }
+        else if (strstr(line, "retornar") != NULL)
+        {
+            strcat(newLine, "return ");
+            strcat(newLine, strchr(line, '0'));
+        }
+        else
+        {
+            strcpy(newLine, line);
+        }
+
+        fputs(newLine, tempFile);
+    }
+
     fclose(filePointer);
+    fclose(tempFile);
+
+    remove("compilator.txt");
+    rename("temp.txt", "compilator.txt");
+
+    printf("La traducción se ha completado.\n");
+    return 0;
 }
